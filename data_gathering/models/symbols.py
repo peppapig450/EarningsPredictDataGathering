0b7f7c35d109785cc7b94f1309ef3c499157d260
peppapig450 <nbgenius1@gmail.com>
2024-05-20 10:29:18 -0700

    docstrings for Symbol class

--- data_gathering/models/symbols.py
+++ data_gathering/models/symbols.py
@@ -3,12 +3,36 @@ import re
 
 
 class Symbol:
+    """
+    A class to represent a finacial instrument.
+
+    Attributes:
+        symbol (str): The symbol representing a finacial instrument.
+    """
+
     INTERNATIONAL_SYMBOL_PATTERN = r"\.[A-Z]*$"
 
     def __init__(self, symbol: str):
-        self.symbol = symbol
+        """
+        Initializes the Symbol instance with the provided symbol.
+
+        Args:
+            symbol (str): The symbol representing a financial instrument.
+        """
+        self.symbol: str = symbol
 
     @classmethod
-    def create(cls, symbol_str: str):
+    def create(cls, symbol_str: str) -> "Symbol":
+        """
+        Creates a Symbol instance if the provided string isn't on an international exchange.
+
+        Args:
+            symbol_str (str): The string to be evaluated and potentially converted into a Symbol instance.
+
+        Returns:
+            Symbol: An instance of the Symbol class if the string does not match the international symbol pattern.
+            None: If the string matches the international symbol pattern.
+        """
         if not re.match(cls.INTERNATIONAL_SYMBOL_PATTERN, symbol_str):
             return cls(symbol_str)
+        return None
