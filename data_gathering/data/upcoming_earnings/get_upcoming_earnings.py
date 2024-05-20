a29f4fb53fbad4e91b00ff40c9e9778316ea6779
peppapig450 <nbgenius1@gmail.com>
2024-05-20 16:51:28 -0700

    make UpcomingEarnings an async generator

--- data_gathering/data/upcoming_earnings/get_upcoming_earnings.py
+++ data_gathering/data/upcoming_earnings/get_upcoming_earnings.py
@@ -2,18 +2,20 @@ import fmpsdk
 from data_gathering.models.symbols import Symbol
 from data_gathering.config.api_keys import APIKeys
 from data_gathering.models.upcoming_earning import UpcomingEarning
+import asyncio
 
 
-# TODO: associate earnings report date with symbol
 class UpcomingEarnings:
-    def __init__(self):
-        api_keys = APIKeys.from_config_file()
+    def __init__(self, api_keys: APIKeys):
         self.api_key = api_keys.fmp_api_key
 
-    def get_upcoming_earnings(self, from_date, to_date):
-        def upcoming_earnings_generator():
-            upcoming_earnings_list = fmpsdk.earning_calendar(
-                apikey=self.api_key, from_date=from_date, to_date=to_date
+    async def get_upcoming_earnings(self, from_date, to_date):
+        async def upcoming_earnings_generator():
+            upcoming_earnings_list = await asyncio.to_thread(
+                fmpsdk.earning_calendar,
+                apikey=self.api_key,
+                from_date=from_date,
+                to_date=to_date,
             )
             for earning in upcoming_earnings_list:
                 if symbol := earning.get("symbol"):
@@ -21,4 +23,5 @@ class UpcomingEarnings:
                     if symbol and (earnings_date := earning.get("date")):
                         yield UpcomingEarning(symbol, earnings_date)
 
-        return upcoming_earnings_generator()
+        async for earnings in upcoming_earnings_generator():
+            yield earnings
