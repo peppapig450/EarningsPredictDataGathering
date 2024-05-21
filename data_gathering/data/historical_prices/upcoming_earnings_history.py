d18a5950bf4821da137c5ebd1b49b6a7d6121bb3
peppapig450 <nbgenius1@gmail.com>
2024-05-20 19:40:24 -0700

    add cache

--- data_gathering/data/historical_prices/upcoming_earnings_history.py
+++ data_gathering/data/historical_prices/upcoming_earnings_history.py
@@ -1,20 +1,42 @@
 import asyncio
 import aiohttp
 import pandas as pd
+import json
 
 from data_gathering.config.api_keys import APIKeys
+from data_gathering.utils import get_logger
 
 
 class HistoricalData:
-    def __init__(self, api_keys: APIKeys, from_date, to_date) -> None:
+    CACHE_FILE = "symbols_cache.json"
+
+    def __init__(self, api_keys: APIKeys, from_date, to_date, semaphore) -> None:
         self.apca_key_id = api_keys.__getattribute__("apca_key_id")
         self.apca_api_secret_key = api_keys.__getattribute__("apca_api_secret_key")
-        self.session = aiohttp.ClientSession(headers=self.get_headers())
         self.from_date = from_date
         self.to_date = to_date
+        self.semaphore = semaphore
+        self.symbols_without_historical_data = self.load_cache_from_file()
         self.base_url = "https://data.alpaca.markets/v2/stocks/bars"
         self.rest_of_link = f"&timeframe=1Day&start={self.from_date}&end={self.to_date}&limit=10000&adjustment=raw&feed=sip&sort=asc"
         self.historical_data_by_symbol = {}
+        self.session = None
+        # self.logger = get_logger(__name__)
+
+    def load_cache_from_file(self):
+        try:
+            with open(self.CACHE_FILE, "r") as file:
+                return set(json.load(file))
+        except FileNotFoundError:
+            return set()
+
+    def save_cache_to_file(self):
+        existing_cache = self.load_cache_from_file()  # Load existing cache
+        updated_cache = existing_cache.union(self.symbols_without_historical_data)
+        with open(
+            self.CACHE_FILE, "w"
+        ) as file:  # Open in "w" mode to overwrite existing contents
+            json.dump(list(updated_cache), file)
 
     def get_headers(self):
         return {
@@ -23,20 +45,40 @@ class HistoricalData:
             "APCA-API-SECRET-KEY": self.apca_api_secret_key,
         }
 
+    async def close(self):
+        if self.session:
+            await self.session.close()
+
+    async def get_session(self):
+        if not self.session:
+            self.session = aiohttp.ClientSession(headers=self.get_headers())
+        return self.session
+
     async def fetch_data(self, symbol):
         url = f"{self.base_url}?symbols={symbol}{self.rest_of_link}"
 
-        async with self.session.get(url) as response:
-            data = await response.json()
+        session = await self.get_session()
+        async with self.semaphore:
+            async with session.get(url) as response:
+                data = await response.json()
 
-            try:
-                data = data["bars"]
-                return data
-            except KeyError:
-                pass
+                if "bars" not in data or not data["bars"]:
+                    # Add symbol to the cache if historical data is empty
+                    self.symbols_without_historical_data.add(symbol)
+                    return None
+
+                try:
+                    data = data["bars"]
+                    return data
+                except KeyError:
+                    # Add symbol to the cache if historical data retrieval fails
+                    return None
 
     async def fetch_historical_data(self, symbol):
-        await asyncio.sleep(1.05)
+        if symbol in self.symbols_without_historical_data:
+            return None
+
+        await asyncio.sleep(0.3)
         data = await self.fetch_data(symbol)
         if data:
             df = self.normalize_and_rename(data, symbol)
@@ -70,5 +112,7 @@ class HistoricalData:
 
         return normalized_df
 
-    async def close(self):
-        await self.session.close()
+    async def finish(self):
+        if self.session:
+            await self.session.close()
+        self.save_cache_to_file()
