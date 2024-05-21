41872f2856efc54a5b447188ef56c2a548c126ac
peppapig450 <nbgenius1@gmail.com>
2024-05-20 19:40:40 -0700

    run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
-from data_gathering.data import fetch_all_data
+import cProfile
 import asyncio
+from data_gathering.data import fetch_all_data
 
 
 async def main():
@@ -7,4 +8,16 @@ async def main():
 
 
 if __name__ == "__main__":
+    # Create a cProfile object
+    profiler = cProfile.Profile()
+    # Enable profiling
+    profiler.enable()
+
+    # Run the main coroutine
     asyncio.run(main())
+
+    # Disable profiling
+    profiler.disable()
+
+    # Save profiling results to a file
+    profiler.dump_stats("profile_results.prof")
