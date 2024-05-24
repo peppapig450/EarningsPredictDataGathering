146e09fa73252b8bc9621243db97b0f93b12cb96
peppapig450 <nbgenius1@gmail.com>
2024-05-24 15:04:15 -0700

    add pylintrc and fix the inits

--- data_gathering/__init__.py
+++ data_gathering/__init__.py
@@ -1,4 +1,3 @@
 from . import config
 from . import test  # Not sure this should be here
 from . import utils
-from .utils.cache.cache import Cache
--- data_gathering/utils/__init__.py
+++ data_gathering/utils/__init__.py
@@ -2,3 +2,4 @@
 from .date_utils import DateUtils
 from .logging import get_logger
 from .output import OutputUtils
+from .cache import cache
