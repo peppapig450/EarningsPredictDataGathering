16f25a34c4eb0d3265652d0c3b7c31adb2e3a720
peppapig450 <nbgenius1@gmail.com>
2024-05-27 14:18:25 -0700

    rewrite task handler to use multiprocessing

--- data_gathering/models/task_handler.py
+++ data_gathering/models/task_handler.py
@@ -1,45 +1,30 @@
 from collections import deque
-from concurrent.futures import ProcessPoolExecutor
-import asyncio
-from data_gathering.models.symbol_iterator import SymbolIterator
+from multiprocessing import Pool, Queue as MPQueue
 from data_gathering.models.task_meta import TaskMeta, TaskType, RunState, DataCategory
 from data_gathering.models.task import Task
 
 
 class TaskHandler:
-    def __init__(self, io_executor, cpu_executor, symbols):
-        self.io_executor = io_executor
-        self.cpu_executor = cpu_executor
-        self.io_queue = deque()
-        self.cpu_queue = deque()  # multi processing queue better
-        self.symbols = symbols
-        self.categories = deque(DataCategory)
-        self.current_category = self.categories[0]
+    def __init__(self, io_pool, cpu_pool, io_queue, cpu_queue, cpu_result_ns):
+        self.io_pool = io_pool
+        self.cpu_pool = cpu_pool
+        self.io_queue = io_queue
+        self.cpu_queue = cpu_queue
+        self.cpu_result_namespace = cpu_result_ns
 
     def add_task(self, task):
         if task.task_type == TaskType.IO:
-            self.io_queue.append(task)
+            self.io_queue.put(task)
         elif task.task_type == TaskType.CPU:
-            self.cpu_queue.append(task)
+            self.cpu_queue.put(task)
 
-    async def io_worker(self):
-        while self.io_queue:
-            task = self.io_queue.popleft()
-            await task.run_io()
-            if task.state == RunState.DONE:
-                next_task = task.data_processor_class(
-                    id=task.id,
-                    io_duration=task.io_duration,
-                    task_type=TaskType.CPU,
-                    data_category=task.data_category,
-                    symbols=task.symbols,
-                )
-                self.add_task(next_task)
+    # TODO: look into using imap
+    def io_worker(self):
+        while not self.io_queue.empty():
+            task = self.io_queue.get()
+            self.io_pool.apply_async(task.run_io, args=(self.cpu_queue,))
 
     def cpu_worker(self):
-        while self.cpu_queue:
-            task = self.cpu_queue.popleft()
-            task.run_cpu()
-            if task.state == RunState.DONE:
-                # CPU processing is done, nothing more to do here for the task
-                pass
+        while not self.cpu_queue.empty():
+            task = self.cpu_queue.get()
+            task.run_cpu(self.cpu_result_namespace)
