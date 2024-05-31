dfa1bbc64096ad42aeaee8a47463e258d990c0c2
peppapig450 <nbgenius1@gmail.com>
2024-05-31 10:39:34 -0700

    add better error handling and add a wrapper ufnction that returns strings

--- data_gathering/data/get_upcoming_earnings.py
+++ data_gathering/data/get_upcoming_earnings.py
@@ -9,6 +9,10 @@ from data_gathering.models.upcoming_earning import UpcomingEarning
 from data_gathering.utils.cache.symbols_blacklist import BlacklistSymbolCache
 
 
+class NoUpcomingEarningsError(Exception):
+    pass
+
+
 class UpcomingEarnings:
     def __init__(self, api_keys: APIKeys, cache: BlacklistSymbolCache):
         self.api_key = api_keys.fmp_api_key
@@ -20,18 +24,27 @@ class UpcomingEarnings:
     ) -> List[UpcomingEarning]:
         payload = {"from": from_date, "to": to_date, "apikey": self.api_key}
         response = requests.get(self.base_url, params=payload, timeout=timeout)
-        if response.status_code == 200:
-            try:
-                data = response.json()
-                parsed_data = [
-                    UpcomingEarning(**item)
-                    for item in data
-                    if not re.search(r"[-.][A-Z]+$", item["symbol"])
-                    and not self.cache.is_blacklisted(item["symbol"])
-                ]
-            except ValidationError as e:
-                raise e.with_traceback(e.__traceback__)
-        else:
-            raise requests.exceptions.RequestException
+        try:
+            response.raise_for_status()
+            data = response.json()
+            parsed_data = [
+                UpcomingEarning(**item)
+                for item in data
+                if not re.search(r"[-.][A-Z]+$", item["symbol"])
+                and not self.cache.is_blacklisted(item["symbol"])
+            ]
+            if not parsed_data:
+                raise RuntimeError from NoUpcomingEarningsError(
+                    "Error while retrieving upcoming earnings list."
+                )
+            return parsed_data
+        except ValidationError as e:
+            raise RuntimeError from e.with_traceback(e.__traceback__)
+        except requests.exceptions.RequestException as e:
+            raise RuntimeError from e
 
-        return parsed_data
+    def get_upcoming_earnings_list_strings(
+        self, from_date: str, to_date: str, timeout=20
+    ) -> List[str]:
+        earnings_list = self.get_upcoming_earnings_list(from_date, to_date, timeout)
+        return [symbols.symbol for symbols in earnings_list]
