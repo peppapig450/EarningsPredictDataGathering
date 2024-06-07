822d5a92db4c4b75091c93eeddf6a31867148734
peppapig450 <nbgenius1@gmail.com>
2024-06-06 19:41:52 -0700

    start working on historical processing class

--- data_gathering/data/historical/historical_processing.py
+++ data_gathering/data/historical/historical_processing.py
@@ -1,10 +1,23 @@
 import pandas as pd
 from collections import defaultdict
+from data_gathering.models.mappings import historical_data_mapping
+
 
 class HistoricalDataProcessing:
-    def __init__(self,
-                 ) -> None:
-        ()
+    # queue later
+    def __init__(self) -> None:
+        self.data_dict = defaultdict(list)
 
+    def rename_columns(self, response_data):
+        for batch in response_data:
+            for symbol, data in batch.items():
+                for i, bar in enumerate(data):
+                    renamed_bar = {
+                        historical_data_mapping[key]: val
+                        for key, val in bar.items()
+                        if key in historical_data_mapping
+                    }
+                    renamed_bar["symbol"] = symbol
+                    data[i] = renamed_bar
 
-    def __i
+                self.data_dict[symbol] = data
