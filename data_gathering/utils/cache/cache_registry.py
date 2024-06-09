f170d772db250ed4cd8e82cd074578f8d459af51
peppapig450 <nbgenius1@gmail.com>
2024-06-08 17:40:37 -0700

    add type hint and doc strings to Cache registry

--- data_gathering/utils/cache/cache_registry.py
+++ data_gathering/utils/cache/cache_registry.py
@@ -1,35 +1,89 @@
 from threading import RLock
+import logging
 from functools import wraps
+from cache import Cache
+from typing import Callable, Type, Any
+
+
+type CacheMethod = Callable[..., Any]
+type CacheDecorator = Callable[..., Any]
+
+logger = logging.getLogger(__name__)
 
 
 class CacheRegistry:
     """Registry to store and retrieve decorated cache objects"""
 
     def __init__(self) -> None:
-        self._cache_objects = {}
-        self._locks = {}
+        self._cache_objects: dict[Type[Cache], Cache] = {}
+        self._locks: dict[Type[Cache], RLock] = {}
 
-    def cache_decorator(self, func):
-        @wraps(func)
-        def decorator(cache_class):
-            def wrapper(*args, **kwargs):
+    def cache_decorator(self, func: CacheMethod) -> Any:
+        """
+        Decorator to wrap functions with cache handling.
+
+        Ensures the cache is synced and closed after the function call.
+
+        :param func: Function to be wrapped
+        :return: Wrapped function
+        """
+
+        def decorator(cache_class: Type[Cache]) -> Any:
+            @wraps(func)
+            def wrapper(*args, **kwargs) -> Any:
                 # Use the registry to get the decorated cache object
-                decorated_func = self.get_cache(cache_class)
-                # Call the decorated function with arguments
-                return decorated_func(*args, **kwargs)
+                cache_obj = self.get_cache(cache_class)
+                try:
+                    # Call the wrapped function with the cache
+                    return func(cache_obj, *args, **kwargs)
+                finally:
+                    # ensure we sync and close the cache
+                    cache_obj.sync()
+                    cache_obj.close()
 
             return wrapper
 
         return decorator
 
-    def get_cache(self, cache_class):
+    def get_cache(self, cache_class: Type[Cache]) -> Cache:
+        """
+        Retrieve or create a cache object of the specified class.
+
+        If a cache object of the specified class does not exist, a new instance is created and returned.
+
+        If the provided class is not a subclass of Cache, a warning is logged.
+
+        :param cache_class: The class of the cache object to retrieve or create.
+        :return: The cache object of the specified class.
+        """
+        if not issubclass(cache_class, Cache):
+            warning_msg = f"{cache_class} is not a subclass of Cache. Caching with this object may not work properly."
+            logger.warning(warning_msg, Warning)
+            return None
+
         with self.get_lock(cache_class):
             if cache_class not in self._cache_objects:
                 cache_obj = cache_class()  # Create a new instance of the cache class
-                decorated_func = self.cache_decorator(cache_obj)(cache_class)
-                self._cache_objects[cache_class] = decorated_func
+                self._cache_objects[cache_class] = cache_obj
             return self._cache_objects[cache_class]
 
-    def get_lock(self, cache_class):
+    def get_lock(self, cache_class: Type[Cache]) -> RLock:
+        """
+        Retrieve or create a lock object associated with the specified cache class.
+
+        If a lock object associated with the specified cache class does not exist, a new RLock instance is created and returned.
+
+        If the provided class is not a subclass of Cache, a warning is logged.
+
+        :param cache_class: The class of the cache object associated with the lock.
+        :return: The lock object associated with the specified cache class.
+        """
+        if not issubclass(cache_class, Cache):
+            warning_msg = f"{cache_class} is not a subclass of Cache. Caching with this object may not work properly."
+            logger.warning(warning_msg, Warning)
+            return (
+                RLock()
+            )  # Return a new RLock if the cache class is not a subclass of Cache
+
         with self._locks.get(cache_class, RLock()):
             return self._locks[cache_class]
