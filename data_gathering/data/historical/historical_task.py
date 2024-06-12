f718c810abbbf7e9b067794c1beb3e7a04848799
peppapig450 <nbgenius1@gmail.com>
2024-06-12 15:00:21 -0700

    start on HistoricalDataTask

--- data_gathering/data/historical/historical_task.py
+++ data_gathering/data/historical/historical_task.py
@@ -1,1 +1,27 @@
-from data_gathering.models.task import Task
+from typing import Any
+
+from data_gathering.models.task import DataCategory, Task, TaskMeta, TaskType
+
+from .historical_gathering import HistoricalDataGathering
+from .historical_data_session import HistoricalDataSessionManager
+from .historical_processing import HistoricalDataProcessing
+
+
+class HistoricalDataTask(Task):
+    def __init__(
+        self,
+        *,
+        task_type: TaskType,
+        data_category: DataCategory = DataCategory.HISTORICAL,
+        symbols: tuple[tuple[Any, ...], int],
+        symbols_seen: int
+    ) -> None:
+        super().__init__(
+            task_type=task_type,
+            data_category=data_category,
+            symbols=symbols,
+            symbols_seen=symbols_seen,
+        )
+        
+    def run_io(self):
+        
