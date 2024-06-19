52750f13f24228b83e5d80d0b821b5e3878bd80e
peppapig450 <nbgenius1@gmail.com>
2024-06-19 16:57:24 -0700

    refactor: write functions for building the base url and the pagination url, seperating concerns and improving clarity

--- data_gathering/data/historical/historical_gathering.py
+++ data_gathering/data/historical/historical_gathering.py
@@ -12,6 +12,7 @@ from data_gathering.config.api_keys import APIKeys, APIService
 
 type window = tuple[Any, ...]
 
+
 # XXX: cache temporarily commented out as its not used.
 # TODO: implement caching to properly blacklist symbols with no data
 class HistoricalDataGathering:
@@ -21,8 +22,7 @@ class HistoricalDataGathering:
         to_date: str,
         session_manager: HistoricalDataSessionManager,
         from_date: str = "1983-01-01",
-       # cache: BlacklistSymbolCache | None = None,
-
+        # cache: BlacklistSymbolCache | None = None,
     ) -> None:
         """
         Initializes the HistoricalDataGathering object.
@@ -39,7 +39,7 @@ class HistoricalDataGathering:
         )
 
         self.from_date, self.to_date = self._validate_and_init_dates(from_date, to_date)
-        #self.cache: BlacklistSymbolCache = cache
+        # self.cache: BlacklistSymbolCache = cache
         self.session_manager: HistoricalDataSessionManager = (
             session_manager  # created by task handler
         )
@@ -59,11 +59,9 @@ class HistoricalDataGathering:
         Raises:
             ValueError: If the date strings do not match the YYYY-MM-DD format.
         """
-        compiled_pattern = re.compile(
-            r"\b\d{4}-\d{2}-\d{2}\b"
-        )
+        compiled_pattern = re.compile(r"\b\d{4}-\d{2}-\d{2}\b")
 
-        #TODO: custom exception
+        # TODO: custom exception
         if not re.match(compiled_pattern, from_date):
             raise ValueError(
                 f"Invalid date string for 'from_date': {from_date}. Use YYYY-MM-DD format."
@@ -75,6 +73,86 @@ class HistoricalDataGathering:
             )
 
         return from_date, to_date
+
+    def _build_alpaca_request_url(self, symbols: list[str]) -> str:
+        """
+        Constructs a request URL for the Alpaca API with the provided stock symbols and
+        predefined query parameters.
+
+        Args:
+            symbols (List[str]): A list of stock symbols to include in the request.
+
+        Raises:
+            ValueError: If no symbols are provided.
+
+        Returns:
+            str: The constructed request URL for the Alpaca API.
+        """
+        # Raise value error if no symbols provided
+        if symbols is None or len(symbols) == 0:
+            logging.error("No symbols passed to url builder.")
+            raise ValueError("No symbols passed to url builder")
+
+        # Base url for our requests, and empty dictionary for query_params
+        base_url: str = "https://data.alpaca.markets/v2/stocks/bars"
+        query_params: dict[str, str] = {}
+
+        # Set the 'symbols' key to a comma seperated list of symbols
+        query_params["symbols"] = ",".join(symbols)
+
+        # Add the other query params to the query_params dict
+        query_params |= {
+            "timeframe": "1Day",
+            "start": self.from_date,
+            "end": self.to_date,
+            "limit": "10000",
+            "asof": date.today().strftime("%Y-%m-%d"),
+            "feed": "sip",
+            "sort": "asc",
+        }
+
+        # Encode the query parameters
+        encoded_url = urlencode(query_params)
+
+        # Construct the complete url
+        complete_url = f"{base_url}?{encoded_url}"
+
+        return complete_url
+
+    def _build_pagination_url(self, url: str, next_page_token: str) -> str:
+        """
+        Constructs a pagination URL by appending the provided next_page_token to the given URL.
+
+        Args:
+            url (str): The base URL.
+            next_page_token (str): The token for the next page of results.
+
+        Returns:
+            str: The constructed pagination URL.
+        """
+        # Parse the url and split the query
+        parsed_url = urlparse(url)
+        query_params = parse_qsl(parsed_url.query)
+
+        # Add the next page token to the url query parameters
+        query_params.append(("page_token", next_page_token))
+
+        # Encode the next query params
+        new_query = urlencode(query_params, doseq=True)
+
+        # Reconstruct the url with the 'page_token' added
+        new_url = urlunparse(
+            (
+                parsed_url.scheme,
+                parsed_url.netloc,
+                parsed_url.path,
+                parsed_url.params,
+                new_query,
+                parsed_url.fragment,
+            )
+        )
+
+        return new_url
 
     def get_headers(self) -> dict[str, str]:
         """
@@ -88,9 +166,14 @@ class HistoricalDataGathering:
             "APCA-API-SECRET-KEY": self._apca_api_secret_key,
         }
 
-    # TODO: figure how to use callable to annotate session
+    # TODO: look into memoization for the urls of the make_api_request and pagination
     # TODO: add symbols that return empty data to blacklist cache
-    async def make_api_request(self, session, symbols: Optional[list[str]] = None, url: Optional[str] = None) -> tuple[dict[str, Any], str]:
+    async def make_api_request(
+        self,
+        session: aiohttp.ClientSession,
+        symbols: Optional[list[str]] = None,
+        url: Optional[str] = None,
+    ) -> tuple[dict[str, Any], str]:
         """
         Makes an API request to the Alpaca service.
 
@@ -107,11 +190,7 @@ class HistoricalDataGathering:
             RuntimeError: If there is an error gathering data.
         """
         if symbols is not None:
-            symbols_batch = ",".join(symbols)
-            encoded_symbols = quote(symbols_batch)
-            base_url: str = "https://data.alpaca.markets/v2/stocks/bars"
-            rest_of_url: str = f"&timeframe=1Day&start={self.from_date}&end={self.to_date}&limit=10000&asof={date.today().strftime("%Y-%m-%d")}&feed=sip&sort=asc"
-            complete_url: str = f"{base_url}?symbols={encoded_symbols}{rest_of_url}"
+            complete_url = self._build_alpaca_request_url(symbols)
         elif url is not None:
             complete_url = url
         else:
@@ -120,14 +199,18 @@ class HistoricalDataGathering:
         try:
             async with session.get(complete_url) as response:
                 data = await response.json()
-                return data, response.url
+                return data, complete_url
         except (aiohttp.ClientConnectionError, aiohttp.ClientResponseError) as err:
-            self.logger.error(f"Error: {err} - occured while retrieving data from {complete_url}", exc_info=True)
-            return None #TODO: raise custom error here instead
+            self.logger.error(
+                f"Error: {err} - occured while retrieving data from {complete_url}",
+                exc_info=True,
+            )
+            return None  # TODO: raise custom error here instead
 
-
-    #XXX: refactor this eventually
-    async def handle_response_pagination(self, session, data, url: str) -> OrderedDict | None:
+    # XXX: refactor this eventually
+    async def handle_response_pagination(
+        self, session, data, url: str
+    ) -> OrderedDict | None:
         """
         Handles the pagination for API responses.
 
@@ -146,37 +229,10 @@ class HistoricalDataGathering:
         data_dict.update(data.get("bars", None))
         next_page_token = data.get("next_page_token", None)
 
-
         while next_page_token:
             self.logger.debug(f"Getting pagination for {url} with {next_page_token}")
 
-            # Parse the url
-            parsed_url = urlparse(url)
-            query_params = parse_qsl(parsed_url.query) # split the query
-
-            # Insert the 'next_page_token' before the 'sort parameter
-            for i, (key, _) in enumerate(query_params):
-                if key == "sort":
-                    query_params.insert(i, ("page_token", next_page_token))
-                    break
-            else:
-                # If 'sort' parameter not found, append the 'next_page_token' at the end
-                query_params.append(("page_token", next_page_token))
-
-            # Encode the next query params
-            new_query = urlencode(query_params, doseq=True)
-
-            # Reconstruct the url with the 'page_token' added
-            new_url = urlunparse(
-                (
-                    parsed_url.scheme,
-                    parsed_url.netloc,
-                    parsed_url.path,
-                    parsed_url.params,
-                    new_query,
-                    parsed_url.fragment
-                )
-            )
+            new_url = self._build_pagination_url(url, next_page_token)
 
             new_data, _ = await self.make_api_request(session, url=new_url)
             data_dict.update(new_data.get("bars", {}))
@@ -184,7 +240,9 @@ class HistoricalDataGathering:
             if data_dict is not None:
                 next_page_token = new_data.get("next_page_token", None)
             else:
-                self.logger.error("Something went wrong with the pagination", stack_info=True)
+                self.logger.error(
+                    "Something went wrong with the pagination", stack_info=True
+                )
                 raise RuntimeError("Error while paginating through the data requests.")
 
         return data_dict
