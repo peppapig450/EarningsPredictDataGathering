9c8525bd3b1e2a89084dc20c1ddcdd8932ca23ee
peppapig450 <nbgenius1@gmail.com>
2024-06-18 17:08:37 -0700

    add the  batch  iterator to the init

--- data_gathering/models/__init__.py
+++ data_gathering/models/__init__.py
@@ -2,6 +2,7 @@ from .async_session_manager import AsyncGenerator
 from .date_range import DateRange, TimeUnit
 from .exceptions import ConfigLoadError, NoUpcomingEarningsError
 from .mappings import historical_data_mapping
+from .symbol_iterator import BatchIteratorWithCount
 from .task_handler import Task, TaskHandler
 from .task_meta import DataCategory, RunState, TaskMeta, TaskType
 from .upcoming_earning import UpcomingEarning
