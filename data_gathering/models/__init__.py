bd8f9c43c8df05bccf7022adc0e5f77bf5bc8633
peppapig450 <nbgenius1@gmail.com>
2024-06-19 11:59:48 -0700

    fix the init to properly export the Task class

--- data_gathering/models/__init__.py
+++ data_gathering/models/__init__.py
@@ -3,7 +3,8 @@ from .date_range import DateRange, TimeUnit
 from .exceptions import ConfigLoadError, NoUpcomingEarningsError
 from .mappings import historical_data_mapping
 from .symbol_iterator import BatchIteratorWithCount
-from .task_handler import Task, TaskHandler
+from .task import Task
+from .task_handler import TaskHandler
 from .task_meta import DataCategory, RunState, TaskMeta, TaskType
 from .upcoming_earning import UpcomingEarning
 from .yaml_objects import CurrentDate
