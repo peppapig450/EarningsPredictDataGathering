7259acd4c57041425a6b7aec76d9dadc10f3d10f
peppapig450 <nbgenius1@gmail.com>
2024-06-20 23:11:05 -0700

    note about figuring out how to get the historical data task working, i think im overcomplicating it.

--- data_gathering/data/historical/historical_task.py
+++ data_gathering/data/historical/historical_task.py
@@ -1,4 +1,7 @@
 from typing import Any
+import asyncio
+import aiohttp
+from .historical_data_session import HistoricalDataSessionManager
 
 from data_gathering.models import DataCategory, Task, TaskMeta, TaskType
 
@@ -7,6 +10,9 @@ from .historical_data_session import HistoricalDataSessionManager
 from .historical_processing import HistoricalDataProcessing
 
 
+# TODO: figure out how to create this based on the DataCategory
+# task factory method in the TaskHandler is the leader rn or figure out how
+# get the proper methods from the data specific classes with the metaclass?
 class HistoricalDataTask(Task):
     def __init__(
         self,
@@ -23,4 +29,20 @@ class HistoricalDataTask(Task):
             symbols_seen=symbols_seen,
         )
 
+    async def _gather_data(
+        self, symbols: list[str], session_manager: HistoricalDataSessionManager
+    ) -> list[dict[str, Any]]:
+        """Gathers data for the specified symbols.
+
+        Args:
+            symbols (list[str]): _description_
+            session_manager (HistoricalDataSessionManager): _description_
+        """
+        data_collector = HistoricalDataGathering()
+        pass
+
     def run_io(self):
+        pass
+
+    def run_cpu(self):
+        pass
