a7f9138136dc337bd763e1792cef5944745a34fc
peppapig450 <nbgenius1@gmail.com>
2024-06-22 15:16:06 -0700

    test dynamic import function

--- test.py
+++ test.py
@@ -1,61 +1,22 @@
-from enum import StrEnum
+from data_gathering.models.task_base import Task
+
+import importlib
+
+module_path = "data_gathering.data.historical.historical_task.HistoricalDataTask"
 
 
-class Categories(StrEnum):
-    HISTORICAL = "HistoricalClass"
-    FUNDAMENTAL = "FundamentalClass"
+def get_class(module_path):
+    parts = module_path.strip(".").split(".")
+
+    module_name = parts[-1]
+    package_path = ".".join(parts[:-1])
+
+    try:
+        module = importlib.import_module(package_path)
+        return getattr(module, module_name)
+    except ModuleNotFoundError as e:
+        raise ImportError(f"Module not found: {package_path}") from e
 
 
-class Meta(type):
-    def __call__(cls, *args, **kwargs):
-        category = kwargs.get("category")
-        category_class_name = Categories(category)
-
-        category_class = next(
-            (
-                subclass
-                for subclass in cls.__subclasses__()
-                if subclass.__name__ == category_class_name
-            ),
-            None,
-        )
-
-        # print(category_class.__dict__)
-        # print(cls.__dict__)
-        if category_class is None:
-            raise TypeError(f"Unknown category class")
-
-        CategoryClass = type(
-            category_class_name,
-            (category_class, cls),
-            {
-                attr: getattr(cls, attr)
-                for attr in cls.__dict__
-                if not callable(getattr(cls, attr))
-            },
-        )
-
-        # Dynamically add methods from category_class to the new class
-        for attr, method in category_class.__dict__.items():
-            if callable(method):
-                setattr(CategoryClass, attr, method)
-
-        return CategoryClass.__new__(*args, **kwargs)
-
-
-class MyClass(metaclass=Meta):
-    def __init__(self, /, category):
-        self.category = category
-
-
-class HistoricalClass(MyClass):
-    def run(self):
-        print("Running historical analysis...")
-
-
-class FundamentalClass(MyClass):
-    pass
-
-
-instance = MyClass(category=Categories.HISTORICAL)
-print(instance.run())
+_class = get_class(module_path)
+print(dir(_class))
