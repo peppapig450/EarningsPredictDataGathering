52f64dcfd36c29f7ed1fef1c2542b5072a88984c
peppapig450 <nbgenius1@gmail.com>
2024-06-22 16:47:32 -0700

    create initial task creator class that will hold a fucnction to dynamically import subclasses and create them

--- data_gathering/models/task_creator.py
+++ data_gathering/models/task_creator.py
@@ -3,4 +3,6 @@ from . import Task
 
 
 class TaskCreator:
+    # TODO: one function use importlib to import the path and get the class
+    # TODO: one function to create the class based on the data category (factory class)
     pass
