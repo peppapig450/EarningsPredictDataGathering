0b6493f375a005eedb0cdc9196186c69184862ee
peppapig450 <nbgenius1@gmail.com>
2024-06-22 12:51:31 -0700

    change the init to reflect the new changes

--- data_gathering/models/__init__.py
+++ data_gathering/models/__init__.py
@@ -3,9 +3,7 @@ from .date_range import DateRange, TimeUnit
 from .exceptions import ConfigLoadError, NoUpcomingEarningsError
 from .mappings import historical_data_mapping
 from .symbol_iterator import BatchIteratorWithCount
-from .task_meta import DataCategory, RunState, TaskMeta, TaskType
-from .task import Task
-
+from .task_base import DataCategory, RunState, Task, TaskType
 from .task_handler import TaskHandler
 from .upcoming_earning import UpcomingEarning
 from .yaml_objects import CurrentDate
