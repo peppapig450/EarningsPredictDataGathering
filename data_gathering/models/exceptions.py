0fae1879241feb8d317e4ed88d53951b6fa89cf1
peppapig450 <nbgenius1@gmail.com>
2024-07-02 11:10:14 -0700

    Remove the noupcomingearnings error from exceptions in models

--- data_gathering/models/exceptions.py
+++ data_gathering/models/exceptions.py
@@ -26,44 +26,6 @@ def requires_message(warning_class):
     return warning_class
 
 
-@requires_message
-class NoUpcomingEarningsError(Exception):
-    """
-    Exception raised when there is an error retrieving the upcoming earnings list.
-
-    Attributes:
-        message (str): Explanation of the error. Defaults to a standard error message indicating
-                       that the parsed data is empty.
-
-    Methods:
-        __init__(self, message):
-            Initializes the NoUpcomingEarningsError with a specific error message.
-            Raises a RuntimeError chained from this exception.
-    """
-
-    def __init__(
-        self,
-        message=None,
-    ):
-        """
-        Constructs the NoUpcomingEarningsError with the given error message.
-
-        Parameters:
-            message (str): Explanation of the error. Defaults to "Error occured while retrieving
-                           upcoming earnings list. parsed_data empty."
-
-        Raises:
-            RuntimeError: An exception that is raised from this NoUpcomingEarningsError.
-        """
-        if message is None:
-            self.message = "Error occured while retrieving upcoming earnings list. parsed_data empty."
-        else:
-            self.message = message
-
-        super().__init__(message)
-        raise RuntimeError from self
-
-
 # TODO: expand this doc string
 class TaskCreationError(Exception):
     """
