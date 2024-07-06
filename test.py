edfabab940e3c9232be8fa51f21b51a8092f26e5
peppapig450 <nbgenius1@gmail.com>
2024-07-05 19:16:55 -0700

    directly using create_task and await result speeds up the code to under a minute, not sure how we can apply this to Task Handler

--- test.py
+++ test.py
@@ -1,25 +1,24 @@
 import asyncio
 import multiprocessing
-from aiomultiprocess import Pool
+import aiomultiprocess
 from queue import Full, Empty
+from typing import Any
 import logging
 import cProfile
 
 
-async def async_function(x, queue: multiprocessing.Queue):
+async def async_function(x):
     logging.info(f"Starting async task for {x}")
-    await asyncio.sleep(0.1)
-    data = x * x
-    queue.put(data)
+    await asyncio.sleep(0.5)
+    return x * x
 
 
-def fibonacci(n, max_depth=20):  # Add max_depth parameter
+def fibonacci(n, max_depth=30):  # Add max_depth parameter
     if n <= 1:
         return n
-    elif n > max_depth:
+    if n > max_depth:
         return fibonacci(max_depth, max_depth) + fibonacci(max_depth - 1, max_depth - 1)
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+    return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 def cpu_function(queue: multiprocessing.Queue):
@@ -34,29 +33,36 @@ def cpu_function(queue: multiprocessing.Queue):
             continue
 
 
+async def aiomultiprocess_pool(io_queue, cpu_queue):
+    """Function to hold the aiomultiprocess implementation while we test ThreadPool"""
+    async with aiomultiprocess.Pool(processes=4) as io_pool:
+        while True:
+            try:
+                task = io_queue.get_nowait()
+                await io_pool.apply(
+                    async_function, kwds={"x": task, "queue": cpu_queue}
+                )
+            except Empty:
+                break
+
+
 async def main():
     with cProfile.Profile() as pr:
         with multiprocessing.Manager() as manager:
-            io_queue = manager.Queue()
             cpu_queue = manager.Queue()
             logger = multiprocessing.log_to_stderr()
             logger.setLevel(logging.INFO)
 
-            for i in range(100):
-                io_queue.put_nowait(i)
+            tasks = set(range(100))
 
             with multiprocessing.Pool(processes=6) as cpu_pool:
                 cpu_pool.apply_async(cpu_function, args=(cpu_queue,))
 
-                async with Pool(processes=4) as io_pool:
-                    while True:
-                        try:
-                            task = io_queue.get_nowait()
-                            await io_pool.apply(
-                                async_function, kwds={"x": task, "queue": cpu_queue}
-                            )
-                        except Empty:
-                            break
+                gatherers = [
+                    asyncio.create_task(async_function(task)) for task in tasks
+                ]
+                for result in gatherers:
+                    cpu_queue.put_nowait(await result)
 
                 cpu_pool.close()
                 cpu_pool.join()
