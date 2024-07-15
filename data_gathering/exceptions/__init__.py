203af9686a53b4735dac97a50f66829c51833b09
peppapig450 <nbgenius1@gmail.com>
2024-07-15 14:05:38 -0700

    write a custom exception for UpcomingEarning to raise if it's initialization fails.

--- data_gathering/exceptions/__init__.py
+++ data_gathering/exceptions/__init__.py
@@ -1,5 +1,5 @@
 from .config_exceptions import ConfigLoadError
-from .upcoming_earnings_exceptions import NoUpcomingEarningsError
+from .upcoming_earnings_exceptions import NoUpcomingEarningsError, UpcomingEarningCreationError
 from .task_exceptions import TaskCreationError
 from .historical_data_exceptions import (
     HistoricalDataError,
