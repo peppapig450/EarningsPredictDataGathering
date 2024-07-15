d0ba001b54f31a5ffcb72e7427fb6fb402de3681
peppapig450 <nbgenius1@gmail.com>
2024-07-15 11:01:55 -0700

    add the DateTuple that UpcomingEarning will also use to date_range in models, as it's a date related functionality, and can be useful for other dataclasses

--- data_gathering/models/date_range.py
+++ data_gathering/models/date_range.py
@@ -1,10 +1,16 @@
-# utils.py
+from dataclasses import dataclass
 from datetime import datetime, timedelta
 from enum import StrEnum
-from typing import Any, Self, 
-from dataclasses import dataclass
+from typing import Any, NamedTuple, Self
 
-class
+
+class DateTuple(NamedTuple):
+    date: datetime
+    date_str: str
+    
+    def __str__(self):
+        return self.date_str
+    
 class TimeUnit(StrEnum):
     DAYS = "days"
     WEEKS = "weeks"
