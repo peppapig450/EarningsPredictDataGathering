94858bede4689abc07ab449ae86eaa46f8dccf4f
peppapig450 <nbgenius1@gmail.com>
2024-07-15 11:08:16 -0700

    move the upcoming earning pydantic model to the earnings package

--- data_gathering/models/__init__.py
+++ data_gathering/models/__init__.py
@@ -2,5 +2,4 @@ from .async_session_manager import AbstractSessionManager
 from .date_range import DateRange, TimeUnit, DateTuple
 from .mappings import historical_data_mapping
 from .symbol_iterator import BatchIteratorWithCount
-from .upcoming_earning import UpcomingEarning
 from .yaml_objects import CurrentDate
