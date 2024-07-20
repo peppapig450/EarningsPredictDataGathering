b69888ff906f25f987e9918c84829e3cc9c93233
peppapig450 <nbgenius1@gmail.com>
2024-07-20 12:23:23 -0700

    fix the duck typed filtered_symbols_cache as a classvar, and return from the filtered cache

--- data_gathering/symbols/symbols_dispatcher.py
+++ data_gathering/symbols/symbols_dispatcher.py
@@ -1,23 +1,25 @@
 from collections import defaultdict
 from ..upcoming_earnings import UpcomingEarning
 from ..protocols import SymbolFilteringProtocol
-from types import MappingProxyType
+
+
 class SymbolsDispatcher:
+    filtered_symbols_cache: dict[str, list[UpcomingEarning]]
+
     def __init__(self):
         self.symbols: dict[str, list[UpcomingEarning]] = defaultdict(list)
-        self.filtered_symbols_cache = dict[str, list[UpcomingEarning]] = {}
-        self.symbols_view: MappingProxyType = MappingProxyType(self.symbols)
-        
+        self.filtered_symbols_cache = {}
+
         # TODO: hold instance of Symbols Filter?
-        
+
     def register_symbols(self, symbols_list: list[UpcomingEarning]):
         """Register the upcoming earning's symbols."""
         for earning in symbols_list:
             self.symbols[earning.currency].append(earning)
-            
+
     def get_symbols(self, filter_protocol: SymbolFilteringProtocol):
         api_name = filter_protocol.get_api_name()
-        
+
         # Check cache first
         if api_name in self.filtered_symbols_cache:
-            # TODO: check for a cache hit first
+            return self.filtered_symbols_cache[api_name]
